631fbef7ed2c59499218619384e985ed7a62d1ff
supes5 <65423914+supes5@users.noreply.github.com>
2026-06-11 13:13:41 -0400

    Add milestones.js file for separating milestone configuration

---
+++ milestones.js
@@ -0,0 +1,7 @@
+// Define your milestones
+const milestones = [
+  { date: new Date("2026-07-15T00:00:00"), label: "Midterms" },
+  { date: new Date("2026-10-20T00:00:00"), label: "Clinical Rotations" },
+  { date: new Date("2027-04-01T00:00:00"), label: "Final Exams" },
+  { date: new Date("2027-08-01T00:00:00"), label: "Graduation" }
+];
