cffded8f6981f8362915c06256f90212676bc966
supes5 <65423914+supes5@users.noreply.github.com>
2026-06-11 13:34:04 -0400

    Add new milestones for Summer 2026

--- milestones.js
+++ milestones.js
@@ -1,7 +1,14 @@
 // Define your milestones
+// { date: new Date("T00:00:00"), label: "" },
 const milestones = [
-  { date: new Date("2026-07-15T00:00:00"), label: "Midterms" },
-  { date: new Date("2026-10-20T00:00:00"), label: "Clinical Rotations" },
-  { date: new Date("2027-04-01T00:00:00"), label: "Final Exams" },
+  { date: new Date("2026-05-22T00:00:00"), label: "ABSN Orientation" },
+  { date: new Date("2026-06-16T00:00:00"), label: "First Clinical Rotation!" },
+  { date: new Date("2026-06-18T00:00:00"), label: "First Exams" },
+  { date: new Date("2026-06-22T00:00:00"), label: "Med/Calc Exam I"},
+  { date: new Date("2026-08-10T00:00:00"), label: "Med/Calc Exam II (In Lab)"},
+  { date: new Date("2026-08-14T00:00:00"), label: "Finished First Semester!" },
+
+  { date: new Date("2026-08-24T00:00:00"), label: "Start of Fall 2026" },
+  
   { date: new Date("2027-08-01T00:00:00"), label: "Graduation" }
 ];
